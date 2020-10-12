1653f58d8a4460ae13d9a41b4be5da69612ccf8a
Jason Summers <ja8son@gmail.com>
2020-10-12 14:18:58 -0700

    Initial Commit

---
+++ Scripts/my_first_R_Script.R
@@ -0,0 +1,6 @@
+1+1
+matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
+example <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
+View(example)
+View(example)
+savehistory("~/Coursera/Scripts/my_first_R_Script.R")
